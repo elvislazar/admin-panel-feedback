2b8293e296c36db3ddbad910778cae2d00408ba0
elvislazar <elvislazar.tj@gmail.com>
2025-12-07 14:05:51 +0530

    Third commit

--- backend/app.js
+++ backend/app.js
@@ -10,7 +10,7 @@ const app = express();
 connectDB();
 app.use(cors());
 app.use(express.json());
-app.use("/api/feedback", feedbackRoutes);
+app.use("/feedback", feedbackRoutes);
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
